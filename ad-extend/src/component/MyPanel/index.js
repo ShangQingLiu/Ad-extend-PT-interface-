7f9398d61271ef37814990d996a3410a1ee9c513
SQL <shangching@live.com>
2019-02-16 13:20:42 +0800

    write I2C test finish

--- ad-extend/src/component/MyPanel/index.js
+++ ad-extend/src/component/MyPanel/index.js
@@ -1,103 +1,105 @@
 import React from 'react';
-import {Panel,
- Row,
- Col,
- Button,
- ButtonGroup,
+import {
+    Panel,
+    Row,
+    Col,
+    Button,
+    ButtonGroup,
     InputGroup,
-FormControl,
-   Alert
+    FormControl,
+    Alert
 } from 'react-bootstrap';
 import Graphic from '../Graphic';
-import{messager,getMessage, receiveMessager} from '../../api'
+import {messager, getMessage, receiveMessager} from '../../api'
 
-class MyPanel extends React.Component{
-    constructor(props){
+class MyPanel extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            form:{
-                slave_address:'',
-                reg_address:'',
-                data:'',
-                read_slave_address:'',
-                read_reg_address:'',
-                read_length:'',
-                receive_data:''
+        this.state = {
+            form: {
+                slave_address: '',
+                reg_address: '',
+                data: '',
+                read_slave_address: '',
+                read_reg_address: '',
+                read_length: '',
+                receive_data: ''
             }
         }
     }
-    handleClick= message =>{
+
+    handleClick = message => {
         console.log("handle");
-        if(message.toString()==='write_i2c' ){
-            messager("write_i2c,"+this.state.form.slave_address+","+this.state.form.reg_address
-            +","+this.state.form.data);
-        }
-        else if(message.toString()==='read_i2c' ){
-            receiveMessager("read_i2c,"+this.state.form.read_slave_address+","+this.state.form.read_reg_address
-                +","+this.state.form.read_length);
+        if (message.toString() === 'write_i2c') {
+            messager("write_i2c," + this.state.form.slave_address + "," + this.state.form.reg_address
+                + "," + this.state.form.data);
+        } else if (message.toString() === 'read_i2c') {
+            messager("read_i2c," + this.state.form.read_slave_address + "," + this.state.form.read_reg_address
+                + "," + this.state.form.read_length);
             var toChange = this.state.form;
             toChange["receive_data"] = getMessage();
-            this.setState({form:toChange});
-        }
-        else{
+            this.setState({form: toChange});
+        } else {
             messager(message);
         }
     };
+
     handleChange(change, event) {
         var toChange = this.state.form;
         toChange[change] = event.target.value;
         this.setState({form: toChange});
     }
+
     render() {
-        return(
+        return (
             <div>
                 {/*<Row className="show-grid">*/}
-                    {/*<Col xs={6} md={4}>*/}
-                        {/*<Panel bsStyle="primary">*/}
-                            {/*<Panel.Heading>*/}
-                                {/*<Panel.Title componentClass="h3">Panel heading</Panel.Title>*/}
-                            {/*</Panel.Heading>*/}
-                            {/*<Panel.Body>Panel content*/}
-                            {/*</Panel.Body>*/}
-                        {/*</Panel>*/}
-                    {/*</Col>*/}
-                    {/*<Col xs={6} md={4}>*/}
-                        {/*<Panel bsStyle="success">*/}
-                            {/*<Panel.Heading>*/}
-                                {/*<Panel.Title componentClass="h3">Serial LED control</Panel.Title>*/}
-                            {/*</Panel.Heading>*/}
-                            {/*<Panel.Body>*/}
-                                {/*<ButtonGroup>*/}
-                                    {/*<Button onClick={()=>this.handleClick('2')}>*/}
-                                        {/*Green*/}
-                                    {/*</Button>*/}
-                                    {/*<Button onClick={()=>this.handleClick('3')}>*/}
-                                        {/*Yellow*/}
-                                    {/*</Button>*/}
-                                    {/*<Button onClick={()=>this.handleClick('1')}>*/}
-                                        {/*Red*/}
-                                    {/*</Button>*/}
-                                {/*</ButtonGroup>*/}
-                            {/*</Panel.Body>*/}
-                        {/*</Panel>*/}
-                    {/*</Col>*/}
-                    {/*<Col xs={6} md={4}>*/}
-                        {/*<Panel bsStyle="info">*/}
-                            {/*<Panel.Heading>*/}
-                                {/*<Panel.Title componentClass="h3">BT LED control</Panel.Title>*/}
-                            {/*</Panel.Heading>*/}
-                            {/*<Panel.Body>*/}
-                                {/*<ButtonGroup>*/}
-                                    {/*<Button onClick={()=>this.handleClick('1')}>*/}
-                                        {/*led on*/}
-                                    {/*</Button>*/}
-                                    {/*<Button onClick={()=>this.handleClick('0')}>*/}
-                                        {/*led off*/}
-                                    {/*</Button>*/}
-                                {/*</ButtonGroup>*/}
-                            {/*</Panel.Body>*/}
-                        {/*</Panel>*/}
-                    {/*</Col>*/}
+                {/*<Col xs={6} md={4}>*/}
+                {/*<Panel bsStyle="primary">*/}
+                {/*<Panel.Heading>*/}
+                {/*<Panel.Title componentClass="h3">Panel heading</Panel.Title>*/}
+                {/*</Panel.Heading>*/}
+                {/*<Panel.Body>Panel content*/}
+                {/*</Panel.Body>*/}
+                {/*</Panel>*/}
+                {/*</Col>*/}
+                {/*<Col xs={6} md={4}>*/}
+                {/*<Panel bsStyle="success">*/}
+                {/*<Panel.Heading>*/}
+                {/*<Panel.Title componentClass="h3">Serial LED control</Panel.Title>*/}
+                {/*</Panel.Heading>*/}
+                {/*<Panel.Body>*/}
+                {/*<ButtonGroup>*/}
+                {/*<Button onClick={()=>this.handleClick('2')}>*/}
+                {/*Green*/}
+                {/*</Button>*/}
+                {/*<Button onClick={()=>this.handleClick('3')}>*/}
+                {/*Yellow*/}
+                {/*</Button>*/}
+                {/*<Button onClick={()=>this.handleClick('1')}>*/}
+                {/*Red*/}
+                {/*</Button>*/}
+                {/*</ButtonGroup>*/}
+                {/*</Panel.Body>*/}
+                {/*</Panel>*/}
+                {/*</Col>*/}
+                {/*<Col xs={6} md={4}>*/}
+                {/*<Panel bsStyle="info">*/}
+                {/*<Panel.Heading>*/}
+                {/*<Panel.Title componentClass="h3">BT LED control</Panel.Title>*/}
+                {/*</Panel.Heading>*/}
+                {/*<Panel.Body>*/}
+                {/*<ButtonGroup>*/}
+                {/*<Button onClick={()=>this.handleClick('1')}>*/}
+                {/*led on*/}
+                {/*</Button>*/}
+                {/*<Button onClick={()=>this.handleClick('0')}>*/}
+                {/*led off*/}
+                {/*</Button>*/}
+                {/*</ButtonGroup>*/}
+                {/*</Panel.Body>*/}
+                {/*</Panel>*/}
+                {/*</Col>*/}
                 {/*</Row>*/}
                 <Row className="show-grid">
                     <Col xs={6} md={4}>
@@ -110,35 +112,32 @@ class MyPanel extends React.Component{
                                     0x
                                 </InputGroup.Addon>
                                 <FormControl
-                                    name="read_slave_address"
                                     placeholder="Slave-address(e.g. 5A)"
                                     aria-label="slave_address"
                                     aria-describedby="slave_address"
-                                    onChange={this.handleClick.bind(this, 'name')}
+                                    onChange={this.handleClick.bind(this, 'read_slave_address')}
                                 />
                                 <FormControl
-                                    name="read_reg_address"
                                     placeholder="Reg-address(e.g. FF)"
                                     aria-label="reg_address"
                                     aria-describedby="reg_address"
-                                    onChange={this.handleChange.bind(this,'name')}
+                                    onChange={this.handleChange.bind(this, 'read_reg_address')}
                                 />
                             </InputGroup>
                             <FormControl
-                                name="read_length"
                                 placeholder="Data length"
                                 aria-label="data"
                                 aria-describedby="data"
-                                onChange={this.handleChange.bind(this,'name')}
+                                onChange={this.handleChange.bind(this, 'read_length')}
                             />
                             <Panel.Body>
                                 <ButtonGroup>
-                                    <Button onClick={()=>this.handleClick('read_i2c')}>
+                                    <Button onClick={() => this.handleClick('read_i2c')}>
                                         Read
                                     </Button>
                                 </ButtonGroup>
                                 <h3>Received Data</h3>
-                                <Alert  varient="success">
+                                <Alert varient="success">
                                     <p>{this.state.form.receive_data}</p>
                                 </Alert>
                             </Panel.Body>
@@ -154,29 +153,26 @@ class MyPanel extends React.Component{
                                     0x
                                 </InputGroup.Addon>
                                 <FormControl
-                                    name="slave_address"
                                     placeholder="Slave-address(e.g. 5A)"
                                     aria-label="slave_address"
                                     aria-describedby="slave_address"
-                                    onChange={this.handleClick.bind(this, 'name')}
+                                    onChange={this.handleClick.bind(this, 'slave_address')}
                                 />
                                 <FormControl
-                                    name="reg_address"
                                     placeholder="Reg-address(e.g. FF)"
                                     aria-label="reg_address"
                                     aria-describedby="reg_address"
-                                    onChange={this.handleChange.bind(this,'name')}
+                                    onChange={this.handleChange.bind(this, 'reg_address')}
+                                />
+                                <FormControl
+                                    placeholder="Data (e.g. 3D)"
+                                    aria-label="data"
+                                    aria-describedby="data"
+                                    onChange={this.handleChange.bind(this, 'data')}
                                 />
                             </InputGroup>
-                            <FormControl
-                                name="data"
-                                placeholder="Data"
-                                aria-label="data"
-                                aria-describedby="data"
-                                onChange={this.handleChange.bind(this,'name')}
-                            />
                             <Panel.Body>
-                                <Button onClick={()=>this.handleClick('write_i2c')}>
+                                <Button onClick={() => this.handleClick('write_i2c')}>
                                     Write
                                 </Button>
                             </Panel.Body>
